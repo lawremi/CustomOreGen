e6823a913892445cf06b4c4001877161f2bfec9e
Michael Lawrence <michafla@gene.com>
2014-05-02 06:25:24 -0700

    disable ATG world height calculation

--- src/main/java/CustomOreGen/Integration/ATG/ATGIntegration.java
+++ src/main/java/CustomOreGen/Integration/ATG/ATGIntegration.java
@@ -1,17 +1,15 @@
 package CustomOreGen.Integration.ATG;
 
 import net.minecraft.world.World;
-import ttftcuts.atg.api.ATGAPI;
-import ttftcuts.atg.api.ATGBiomes;
 
 public class ATGIntegration {
 
 	public static boolean worldIsATG(World world) {
-		return ATGAPI.WorldIsATG(world);
+		return false; //ATGAPI.WorldIsATG(world);
 	}
 
 	public static int getSurfaceHeight(World world, int x, int z) {
-		double normalizedHeight = ATGBiomes.getGeneratorInfo(x, z).get(0);
+		double normalizedHeight = 0; //ATGBiomes.getGeneratorInfo(x, z).get(0);
 		return (int)(normalizedHeight * world.getHeight());
 	}
 
