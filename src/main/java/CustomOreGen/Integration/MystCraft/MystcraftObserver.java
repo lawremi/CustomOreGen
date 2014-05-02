ca63ff44858c62a21949f874c85af13d3c23f0cf
Michael Lawrence <michafla@gene.com>
2014-05-02 07:35:32 -0700

    disable listening to DenseOresEvent; no Mystcraft yet

--- src/main/java/CustomOreGen/Integration/MystCraft/MystcraftObserver.java
+++ src/main/java/CustomOreGen/Integration/MystCraft/MystcraftObserver.java
@@ -1,16 +1,11 @@
 package CustomOreGen.Integration.MystCraft;
 
-import java.util.logging.Level;
-
 import net.minecraft.server.MinecraftServer;
-import net.minecraftforge.event.ForgeSubscribe;
-import CustomOreGen.CustomOreGenBase;
 import CustomOreGen.Server.ServerState;
 import CustomOreGen.Server.WorldConfig;
 import CustomOreGen.Server.WorldGenSubstitution;
 import CustomOreGen.Util.BlockDescriptor;
-
-import com.xcompwiz.mystcraft.api.event.DenseOresEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class MystcraftObserver {
 	private static MystcraftObserver _instance = new MystcraftObserver();
@@ -22,8 +17,8 @@ public class MystcraftObserver {
 	public static MystcraftObserver instance() {
 		return _instance;
 	}
-	
-    @ForgeSubscribe
+	/*
+    @SubscribeEvent
     public void onDenseOres(DenseOresEvent event) {
     	ServerState.checkIfServerChanged(MinecraftServer.getServer(), event.worldObj.getWorldInfo());
     	WorldConfig config = ServerState.getWorldConfig(event.worldObj);
@@ -34,4 +29,5 @@ public class MystcraftObserver {
     	subDesc.add(desc, 0.33F);
     	substitution.populate(event.worldObj, event.xPos, event.zPos);
     }
+    */
 }
