30a57a48d9dd1eda93e680ee390c17667370bd74
Michael Lawrence <michafla@gene.com>
2014-05-17 13:58:41 -0700

    restore ATG integration

--- src/main/java/CustomOreGen/Integration/ATG/ATGIntegration.java
+++ src/main/java/CustomOreGen/Integration/ATG/ATGIntegration.java
@@ -1,15 +1,17 @@
 package CustomOreGen.Integration.ATG;
 
 import net.minecraft.world.World;
+import ttftcuts.atg.api.ATGAPI;
+import ttftcuts.atg.api.ATGBiomes;
 
 public class ATGIntegration {
 
 	public static boolean worldIsATG(World world) {
-		return false; //ATGAPI.WorldIsATG(world);
+		return ATGAPI.WorldIsATG(world);
 	}
 
 	public static int getSurfaceHeight(World world, int x, int z) {
-		double normalizedHeight = 0; //ATGBiomes.getGeneratorInfo(x, z).get(0);
+		double normalizedHeight = ATGBiomes.getGeneratorInfo(world, x, z)[0];
 		return (int)(normalizedHeight * world.getHeight());
 	}
 
