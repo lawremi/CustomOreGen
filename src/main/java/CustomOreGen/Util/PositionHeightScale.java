4f403e394a7b7d1c479f8cce743fdf4c92568a68
larman <michafla@gene.com>
2016-10-10 20:17:35 -0700

    drop support for ATG (unlikely to update)

--- src/main/java/CustomOreGen/Util/PositionHeightScale.java
+++ src/main/java/CustomOreGen/Util/PositionHeightScale.java
@@ -1,18 +1,11 @@
 package CustomOreGen.Util;
 
-import CustomOreGen.Integration.ATG.ATGIntegration;
-import cpw.mods.fml.common.Loader;
 import net.minecraft.world.World;
 
 public class PositionHeightScale implements HeightScale {
 
 	@Override
 	public int getHeight(World world, int x, int z) {
-		if (Loader.isModLoaded("ATG")) {
-			if (ATGIntegration.worldIsATG(world)) {
-				return ATGIntegration.getSurfaceHeight(world, x, z);
-			}
-		}
 		return new BiomeHeightScale().getHeight(world, x, z);
 	}
 
