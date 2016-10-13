412e084b5a1dda1fd3579c65b0e5985e2323683a
larman <michafla@gene.com>
2016-10-13 08:01:36 -0700

    use SoundEvent constants

--- src/main/java/CustomOreGen/Server/GuiCustomOreGenSettings.java
+++ src/main/java/CustomOreGen/Server/GuiCustomOreGenSettings.java
@@ -380,7 +380,7 @@ public class GuiCustomOreGenSettings extends GuiScreen
                     if (control.getControl().mousePressed(mc, mouseX, mouseY))
                     {
                         this._currentButton = control.getControl();
-                        mc.getSoundHandler().playSound(PositionedSoundRecord.getMasterRecord(new SoundEvent(new ResourceLocation("gui.button.press")), 1.0F));
+                        mc.getSoundHandler().playSound(PositionedSoundRecord.getMasterRecord(SoundEvents.UI_BUTTON_CLICK, 1.0F));
                         break;
                     }
                 }
@@ -540,7 +540,7 @@ public class GuiCustomOreGenSettings extends GuiScreen
 
             if (control.getControl().mousePressed(mc, mouseX, mouseY))
             {
-            	mc.getSoundHandler().playSound(PositionedSoundRecord.getMasterRecord(new SoundEvent(new ResourceLocation("gui.button.press")), 1.0F));
+            	mc.getSoundHandler().playSound(PositionedSoundRecord.getMasterRecord(SoundEvents.UI_BUTTON_CLICK, 1.0F));
                 this._clickTarget = control;
             }
         }
